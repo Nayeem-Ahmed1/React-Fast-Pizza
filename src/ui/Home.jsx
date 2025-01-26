81ee8df57690c450d035d50bc8362895e3be3de8
Nayeem-Ahmed1 <nabil97773@gmail.com>
2025-01-27 03:28:13 +0600

    favicon

--- src/ui/Home.jsx
+++ src/ui/Home.jsx
@@ -1,6 +1,9 @@
+import { useSelector } from 'react-redux';
 import CreateUser from './../features/user/CreateUser';
 
 function Home() {
+  const cart = useSelector((state) => state.cart.cart);
+
   return (
     <div className="my-10 text-center">
       <h1 className="mb-8 text-xl font-semibold md:text-3xl">
@@ -12,6 +15,11 @@ function Home() {
       </h1>
 
       <CreateUser />
+      {cart.length < 1 && (
+        <p className="text-md absolute bottom-0 left-0 right-0 m-auto font-bold">
+          Made with coffee©Nayeem
+        </p>
+      )}
     </div>
   );
 }
